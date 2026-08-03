5450561deed309a51b80f5910996fe716b02be43
res-mitha <resmitha87@gmail.com>
2026-08-03 07:02:11 +0530

    Time: 0 ms (100%), Space: 46.8 MB (24.97%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int left=0;
+        for(int right = 0;right<nums.length;right++){
+            if(nums[left]!=nums[right]){
+                left++;
+                nums[left]=nums[right];
+               
+            }
+            
+        }
+        return left+1;
+    }
+}
